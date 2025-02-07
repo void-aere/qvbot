a77a7e9110268da1eeeb1097b92ebc63a7c51db3
Jacob Pratt <jpratt13@saddleback.edu>
2025-02-06 21:20:22 -0800

    minimal viable bot

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 before building, install [dpp](https://dpp.dev)
 
+init cmake from `build/` with:
+`cmake ..`
+
 to build, run from base directory:
 `cmake --build build/ -j<thread-count>`
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
-#include <iostream>
+#include <dpp/cluster.h>
+#include <dpp/dispatcher.h>
+#include <dpp/dpp.h>
+#include <dpp/once.h>
+
+#include "../secrets.hpp"
 
 int main() {
 	std::printf("Hello World\n");
+
+    dpp::cluster bot(TOKEN);
+
+    bot.on_log(dpp::utility::cout_logger());
+
+    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+        if (event.command.get_command_name() == "ping") {
+            event.reply("pong");
+        }
+    });
+
+    bot.on_ready([&bot](const dpp::ready_t& event) {
+        if (dpp::run_once<struct register_bot_commands>()) {
+            bot.global_command_create(dpp::slashcommand("ping", "ping pong", bot.me.id));
+        }
+    });
+
+    bot.start(dpp::st_wait);
 }
